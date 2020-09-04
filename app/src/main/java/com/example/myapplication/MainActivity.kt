f200daf2e8fb99bfe0f67095d81dc63e81daf907
siravit2020 <mai_za_2010@hotmail.com>
2020-09-04 23:14:56 +0700

    4

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,5 +9,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val p = com.mai.progressdialog.ProgressDialog(applicationContext,layoutInflater)
+        p.maiDialog()
+
     }
 }
